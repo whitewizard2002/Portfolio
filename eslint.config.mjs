fb6929ecf3e80b535e1ce065c013c555773df438
Aadit Harshal Baldha <aaditbaldha2002@gmail.com>
2024-11-17 23:04:42 -0500

    fix for the ruleset

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -11,7 +12,7 @@ export default [
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
-    plugins: ['react-hooks'],
+    plugins: { 'react-hooks': reactHooks },
     rules: {
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 0,
